194646d37b878d350c29de4c5e50b3f3dffc512f
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-06-23 14:20:15 -0400

    List Products

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,10 +1,34 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main> List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <a href={`/product/${product.slug}`}>
+                  <p>
+                    <strong>{product.price}</strong>
+                  </p>
+                </a>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
