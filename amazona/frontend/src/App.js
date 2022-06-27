bbdba2d2a8459887a7509cd4b329ead850dd40df
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-06-27 12:50:29 -0400

    Create Product Component

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import { Link } from "react-router-dom";
+
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
